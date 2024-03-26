bbe48c4e5b83365b74a5f79fecce7a0b1cb9900a
hans-jacob Melby <hans-jacob.melby@nav.no>
2024-03-26 12:19:22 +0100

    endrer deployment strategi og liveness test

--- src/main/kotlin/no/nav/medlemskap/sykepenger/brukersporsmaalhandler/nais/NaisLiveness.kt
+++ src/main/kotlin/no/nav/medlemskap/sykepenger/brukersporsmaalhandler/nais/NaisLiveness.kt
@@ -19,8 +19,8 @@ fun Routing.naisRoutes(
         if (consumeJob.state().isRunning) {
             call.respondText("Alive!", ContentType.Text.Plain, HttpStatusCode.OK)
         } else {
-            logger.warn("isAlive returnerer HTTP 500. Consumejob sin status er  ${consumeJob.state().name} ")
-            call.respondText("Not alive :(", ContentType.Text.Plain, HttpStatusCode.InternalServerError)
+            logger.warn("Consumejob sin status er  ${consumeJob.state().name} ")
+            call.respondText("Alive!", ContentType.Text.Plain, HttpStatusCode.OK)
         }
     }
     get("/isReady") {
